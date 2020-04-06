d40b8620274c358c52d818ea86d3f61e5ef884ee
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2020-04-06 18:23:49 +0200

    Solution for Iterator

--- Lab3/Iterator/iterator.py
+++ Lab3/Iterator/iterator.py
@@ -1,5 +1,44 @@
+class Counter:
+    def __init__(self, func):
+        self.__func = func
+        self.__counter = 0
+
+    def __call__(self, *args, **qwargs):
+        self.__counter += 1
+        print(f'{self.__counter}. ', end="")
+        return self.__func(self, *args, **qwargs)
+
+
+@Counter
+def printing(self):
+    print("Fibbonacci: ", end="")
+
+
+class FibonacciIterator():
+    def __init__(self, max_val):
+        self.__a = 0
+        self.__b = 1
+        self.__max_val = max_val
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        printing()
+        self.__b = self.__a + self.__b
+        self.__a = self.__b - self.__a
+
+        if self.__b >= self.__max_val:
+            print("Koniec")
+            raise StopIteration
+        return self.__b
+
+
 def main():
-    pass
+    fib = FibonacciIterator(100)
+    it = iter(fib)
+    for f in it:
+        print(f)
 
 
 if __name__ == "__main__":
