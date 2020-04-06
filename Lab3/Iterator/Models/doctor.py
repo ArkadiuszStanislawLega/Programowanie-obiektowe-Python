7b3c44a40684abce0b6bb12931175f64d4bc46cd
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2020-04-06 17:23:50 +0200

    Remove unnecessary files.

--- Lab3/Iterator/Models/doctor.py
+++
@@ -1,7 +0,0 @@
-from .person import Person
-from .medical_specialization import MedicalSpecialization
-
-
-class Doctor(Person):
-    def __init__(self):
-        self.__medical_specialization = MedicalSpecialization.doctor
--- Lab3/Iterator/Models/person.py
+++
@@ -1,7 +0,0 @@
-class Person:
-    def __init__(self):
-        self.__id = ""
-        self.__name = ""
-        self.__surname = ""
-        self.__phone = ""
-        self.__email = ""
