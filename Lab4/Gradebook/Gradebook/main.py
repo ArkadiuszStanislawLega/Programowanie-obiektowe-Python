f717a5a9899a34cc4c61c900f309926b2dab447f
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2020-05-06 17:47:21 +0200

    Code review.

--- Lab4/Gradebook/Gradebook/main.py
+++ Lab4/Gradebook/Gradebook/main.py
@@ -1,14 +1,6 @@
-from Models.school import School
-from Models.grade import Grade
-from Models.student import Student
 from Models.school_class import SchoolClass
-from Views.grade_view import GradeView
-from Views.student_view import StudentView
 from Views.school_class_view import SchoolClassView
-from Controllers.grade_controller import GradeController
-from Controllers.student_controller import StudentController
 from Controllers.school_class_controller import SchoolClassController
-from Enums.grade_type import GradeType
 
 
 def main():
