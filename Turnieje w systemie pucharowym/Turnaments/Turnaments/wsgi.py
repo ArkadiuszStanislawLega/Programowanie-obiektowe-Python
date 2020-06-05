f2e2e4d913b18e76aa22dd54040660da55624b73
ArkadiuszStanislaLega <arkadiusz.stanislaw.lega@gmail.com>
2020-06-05 13:27:51 +0200

    Start app.

--- Turnieje w systemie pucharowym/Turnaments/Turnaments/wsgi.py
+++ Turnieje w systemie pucharowym/Turnaments/Turnaments/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Turnieje project.
+WSGI config for Turnaments project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Turnieje.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Turnaments.settings')
 
 application = get_wsgi_application()
--- Turnieje w systemie pucharowym/Turnaments/manage.py
+++ Turnieje w systemie pucharowym/Turnaments/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Turnieje.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Turnaments.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
